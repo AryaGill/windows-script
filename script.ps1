ba26b0a072410be5e4a51e98e6c12d290063f867
AryaGill <75190244+AryaGill@users.noreply.github.com>
2022-11-08 16:56:25 -0800

    added firewall settings

--- script.ps1
+++ script.ps1
@@ -258,3 +258,35 @@ auditpol /set /category:"System" /failure:enable
     cmd.exe /c 'sc start wscsvc'
     cmd.exe /c 'sc config wscsvc start= auto'
     
+  #firewall settings
+    Write-Warning "Hoisting up the firewall"
+    Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+	Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Allow -NotifyOnListen True -AllowUnicastResponseToMulticast True -LogFileName %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log
+    netsh advfirewall import "C:\Users\$user\Desktop\Win10Firewall.wfw"
+    netsh advfirewall firewall set rule name="Remote Assistance (DCOM-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (PNRP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (RA Server TCP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (SSDP TCP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (SSDP UDP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Remote Assistance (TCP-In)" new enable=no 
+    netsh advfirewall firewall set rule name="Telnet Server" new enable=no 
+    netsh advfirewall firewall set rule name="netcat" new enable=no
+    #disable network discovery hopefully
+    netsh advfirewall firewall set rule group="Network Discovery" new enable=No
+    #disable file and printer sharing hopefully
+    netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=No
+
+    netsh advfirewall firewall add rule name="block_RemoteRegistry_in" dir=in service="RemoteRegistry" action=block enable=yes
+    netsh advfirewall firewall add rule name="block_RemoteRegistry_out" dir=out service="RemoteRegistry" action=block enable=yes
+    
+    New-NetFirewallRule -DisplayName "sshTCP" -Direction Inbound -LocalPort 22 -Protocol TCP -Action Block #ssh
+    New-NetFirewallRule -DisplayName "ftpTCP" -Direction Inbound -LocalPort 21 -Protocol TCP -Action Block #ftp
+    New-NetFirewallRule -DisplayName "telnetTCP" -Direction Inbound -LocalPort 23 -Protocol TCP -Action Block #telnet
+    New-NetFirewallRule -DisplayName "SMTPTCP" -Direction Inbound -LocalPort 25 -Protocol TCP -Action Block #SMTP
+    New-NetFirewallRule -DisplayName "POP3TCP" -Direction Inbound -LocalPort 110 -Protocol TCP -Action Block #POP3
+    New-NetFirewallRule -DisplayName "SNMPTCP" -Direction Inbound -LocalPort 161 -Protocol TCP -Action Block #SNMP
+    New-NetFirewallRule -DisplayName "RDPTCP" -Direction Inbound -LocalPort 3389 -Protocol TCP -Action Block #RDP
+
+    #network profile to public so it denies file sharing, device discovery, etc.
+    Set-NetConnectionProfile -NetworkCategory Public
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\010103000F0000F0010000000F0000F0C967A3643C3AD745950DA7859209176EF5B87C875FA20DF21951640E807D7C24" -Name "Category" -ErrorAction SilentlyContinue
